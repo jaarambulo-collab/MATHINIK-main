131af79fde65da0d4f7d83f000a2af3b98f8b20b
jaarambulo-collab <jaarambulo@ccc.edu.ph>
2025-09-27 19:55:55 +0800

    Pt4

--- Assets/Mathik Script/Canvas.cs
+++ Assets/Mathik Script/Canvas.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject canvas;
-
+    
     void Start()
     {
+        
         // Optionally, hide the canvas at the start
         if (canvas != null)
         {
             canvas.SetActive(true);
+            
         }
     }
     
